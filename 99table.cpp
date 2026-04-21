919ce17278a070ed9fcfc4b6dd2ddc9cc4ffe9f3
s1130298-ui <s1130298@dmhs.kh.edu.tw>
2026-04-21 11:38:49 +0800

    Create 99table.cpp

---
+++ 99table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <=9; i++) {
+		for (int j = 1; j <= 3; j++) {
+			cout << j << j << "*" << i << "=" << j*i <<
+		}
+		cout << "\n";
+	}
+	cout << "-----------------";
+	for (int i = 1; i <=9; i++) {
+			for (int j = 7; j <= 9; j++) {
+				cout << j << j << "*" << i << "=" << j*i <<
+			}
+			cout << "\n";
+		}
+	    cout << "\n";
+		cout << "-----------------";
+	return 0;
+}
